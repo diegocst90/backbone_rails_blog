97b35ab2d11051a724a15ed089bfe4ad73522724
Diego Castillo Rios <diego.cst90@gmail.com>
2012-07-27 14:34:24 -0500

    First code for the Backbone Rails Blog App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '72e51889c3f10d63fd86bf8fbc6bce4ffaaa49183419ace86e9d048db43b71538f9d919cf2b5623788f940552bd11e0d59cea0fd5c9db6bc4110ad9dca727448'
